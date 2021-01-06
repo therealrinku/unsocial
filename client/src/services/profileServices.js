87504d9bd77d87fa4a83f81f259cb97000881676
rinkuuu <backupfb2032@gmail.com>
2021-01-06 17:16:09 +0545

    get saved posts service added

--- client/src/services/profileServices.js
+++ client/src/services/profileServices.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 import server_url from "../server_url";
 
+export const getProfileSavedPosts = async (current_user_uid) => {
+  try {
+    const profileSavedPosts = await axios
+      .get(server_url + `/post/savedposts/${current_user_uid}`)
+      .then((res) => {
+        return res.data;
+      });
+    return profileSavedPosts;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const getProfilePosts = async (current_user_uid, profile_user_uid) => {
   try {
     const profilePosts = await axios
