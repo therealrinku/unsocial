916945d7eb9971aaaca44d1fe9419dfcc97c2d59
rinkuuu <backupfb2032@gmail.com>
2021-01-06 16:54:27 +0545

    follow and unfollow route added

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,146 @@
 const db = require("../database/db");
 const router = require("express").Router();
 
+//unfollow
+const RemoveFollowing = (unfollowing_user_uid, unfollower_user_uid) => {
+  return new Promise((resolve) => {
+    db.query(
+      `UPDATE users SET following=array_remove(following,'${unfollowing_user_uid}')
+		 WHERE uid='${unfollower_user_uid}'`,
+      (err, res0) => {
+        if (!err) {
+          resolve("done");
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+};
+
+const RemoveFollower = (unfollowing_user_uid, unfollower_user_uid) => {
+  return new Promise((resolve) => {
+    db.query(
+      `UPDATE users SET followers=array_remove(followers,'${unfollower_user_uid}')
+		   WHERE uid='${unfollowing_user_uid}'`,
+      (err, res0) => {
+        if (!err) {
+          resolve("done");
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+};
+
+router.post("/unfollow", (req, res) => {
+  RemoveFollowing(req.body.unfollowing_user_uid, req.body.unfollower_user_uid)
+    .then((res0) => {
+      if (res0 === "done") {
+        RemoveFollower(
+          req.body.unfollowing_user_uid,
+          req.body.unfollower_user_uid
+        )
+          .then((res1) => {
+            if (res1 === "done") {
+              res.send("done");
+            }
+          })
+          .catch((err1) => {
+            throw err1;
+          });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+
+//follow
+const AddFollowing = (following_user_uid, follower_user_uid) => {
+  return new Promise((resolve) => {
+    db.query(
+      `UPDATE users SET following=array_append(following,'${following_user_uid}')
+		 WHERE uid='${follower_user_uid}'`,
+      (err, res0) => {
+        if (!err) {
+          resolve("done");
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+};
+
+const AddFollower = (following_user_uid, follower_user_uid) => {
+  return new Promise((resolve) => {
+    db.query(
+      `UPDATE users SET followers=array_append(followers,'${follower_user_uid}')
+		   WHERE uid='${following_user_uid}'`,
+      (err, res0) => {
+        if (!err) {
+          resolve("done");
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+};
+
+router.post("/follow", (req, res) => {
+  AddFollowing(req.body.following_user_uid, req.body.follower_user_uid)
+    .then((res0) => {
+      if (res0 === "done") {
+        AddFollower(req.body.following_user_uid, req.body.follower_user_uid)
+          .then((res1) => {
+            if (res1 === "done") {
+              res.send("done");
+            }
+          })
+          .catch((err1) => {
+            throw err1;
+          });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+
 //get current user data
 router.get("/loggeninuserinfo/:username", (req, res) => {
-	db.query(
-		`SELECT username,uid,profile_image_url FROM users WHERE username='${req.params.username}'`,
-		(err, res0) => {
-			if (err) {
-				throw err;
-			} else {
-				res.send(res0.rows);
-			}
-		}
-	);
+  db.query(
+    `SELECT username,uid,profile_image_url FROM users WHERE username='${req.params.username}'`,
+    (err, res0) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(res0.rows);
+      }
+    }
+  );
 });
 
 //get visited profile info
 router.get("/visiteduserinfo/:username/:current_user_uid", (req, res) => {
-	db.query(
-		`SELECT username,uid,profile_image_url,
+  db.query(
+    `SELECT username,uid,profile_image_url,
     (SELECT COUNT(*) FROM posts WHERE owner_uid=(uid)::text)::int AS posts_count,
     '${req.params.current_user_uid}'=ANY(followers) AS followed_by_me,
     array_length(followers,1) as followers_count,
     array_length(following,1) as following_count
     FROM users WHERE username='${req.params.username}'`,
-		(err, res0) => {
-			if (err) {
-				throw err;
-			} else {
-				res.send(res0.rows);
-			}
-		}
-	);
+    (err, res0) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(res0.rows);
+      }
+    }
+  );
 });
 
 module.exports = router;
