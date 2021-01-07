a0f76d245fead147df5da648cd89be5ff76e75ad
rinkuuu <backupfb2032@gmail.com>
2021-01-07 06:28:27 +0545

    get followers and following service added

--- client/src/services/profileServices.js
+++ client/src/services/profileServices.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 import server_url from "../server_url";
 
+export const getFollowingList = async (username) => {
+  try {
+    const following = await axios
+      .post(server_url + `/user/followings/${username}`)
+      .then((res) => {
+        return res.data;
+      });
+    return following;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export const getFollowersList = async (username) => {
+  try {
+    const followers = await axios
+      .post(server_url + `/user/followers/${username}`)
+      .then((res) => {
+        return res.data;
+      });
+    return followers;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const followUser = async (following_user_uid, follower_user_uid) => {
   try {
     await axios.post(server_url + "/user/follow", {
