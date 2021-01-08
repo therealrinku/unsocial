2a67d0feb89c426c7942a6f95177acaa4bc43c2e
rinkuuu <backupfb2032@gmail.com>
2021-01-08 19:16:17 +0545

    posts reducer connected to main store

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import feedReducer from "./feed/feedReducer";
 import userReducer from "./user/userReducer";
 import profileReducer from "./profile/profileReducer";
+import postsReducer from "./posts/postsReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   feed: feedReducer,
   profile: profileReducer,
+  posts: postsReducer,
 });
 
 export default rootReducer;
