55c0532c590331d73ccaea0481ec03ffd5ba0b6f
rinkuuu <backupfb2032@gmail.com>
2021-01-08 07:58:20 +0545

    post view page style added

--- client/src/pages/PostView.js
+++ client/src/pages/PostView.js
@@ -1,35 +1,92 @@
 import React, { Fragment } from "react";
 import { BiDotsHorizontal } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import * as Icons from "../Icons/CustomIcons";
+import Navbar from "../components/Navbar";
+import MobileNavbar from "../components/MobileNavbar";
 
 const PostView = ({
   poster_profileImage,
   poster_username,
   setShowPostOptionsModal,
   toggleModal,
+  likeUnlikePost,
+  getLikers,
+  post_likesCount,
+  post_postedDate,
+  post_status,
+  haveILiked,
+  haveISaved,
+  saveUnsavePost,
+  post_commentsCount,
 }) => {
   return (
     <Fragment>
+      <Navbar />
+      <MobileNavbar />
       <div className="post--view--">
-        <div>
+        <section>
           <img src="https://bit.ly/2XnWuDE" alt="img" />
-        </div>
+        </section>
 
-        <div>
-          <ul>
-            <img
-              src={poster_profileImage || "https://bit.ly/2XnWuDE"}
-              alt="post_user_image"
-            />
-            <Link to={`/${poster_username}`}>{poster_username || "rinku"}</Link>
-          </ul>
+        <section>
+          <div className="top--section">
+            <ul>
+              <img
+                src={poster_profileImage || "https://bit.ly/2XnWuDE"}
+                alt="post_user_image"
+              />
+              <Link to={`/${poster_username}`}>
+                {poster_username || "rinku"}
+              </Link>
 
-          <ul>
-            <button onClick={() => toggleModal(setShowPostOptionsModal)}>
-              <BiDotsHorizontal />
-            </button>
-          </ul>
-        </div>
+              <button>Follow</button>
+            </ul>
+
+            <ul>
+              <button onClick={() => toggleModal(setShowPostOptionsModal)}>
+                <BiDotsHorizontal />
+              </button>
+            </ul>
+          </div>
+
+          <div className="comment--view-section"></div>
+
+          <div className="buttons">
+            <div className="buttons--section-one">
+              <button onClick={likeUnlikePost}>
+                {haveILiked ? <Icons.LovedIcon /> : <Icons.LoveIcon />}
+              </button>
+
+              <button>
+                <Icons.CommentIcon />
+              </button>
+
+              <button>
+                <Icons.ShareIcon />
+              </button>
+            </div>
+
+            <div>
+              <button onClick={saveUnsavePost}>
+                {haveISaved ? <Icons.SavedIcon /> : <Icons.SaveIcon />}
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <p>{post_status}</p>
+          </div>
+
+          <div>
+            <button onClick={getLikers}>{post_likesCount || "No"} likes</button>
+            <button>{post_commentsCount || 0} comments</button>
+          </div>
+
+          <div className="date">
+            <p>{post_postedDate || "date"}</p>
+          </div>
+        </section>
       </div>
     </Fragment>
   );
