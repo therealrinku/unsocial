e7ae6f914ecaab160b80377c4321e099f55b2a24
rinkuuu <backupfb2032@gmail.com>
2021-01-12 20:55:13 +0545

    signup and hashing password done

--- routes/auth.js
+++ routes/auth.js
@@ -9,28 +9,31 @@ router.post("/login", (req, res) => {
 
 //signup user
 router.post("/signup", (req, res) => {
-  db.query(`SELECT username FROM users WHERE username`, (err, res1) => {
-    if (res1.rowCount <= 0) {
-      bcrypt.hash(req.body.password, 10, (err1, hash) => {
-        if (!err) {
-          db.query(
-            `INSERT INTO users(email,username,password,joined_date,profile_image_url)
+  db.query(
+    `SELECT username FROM users WHERE username='${req.body.username}'`,
+    (err, res1) => {
+      if (res1.rows.length <= 0) {
+        bcrypt.hash(req.body.password, 10, (err1, hash) => {
+          if (!err) {
+            db.query(
+              `INSERT INTO users(email,username,password,joined_date,profile_image_url)
           VALUES('${req.body.email}','${
-              req.body.username
-            }','${hash}','${new Date()}','https://ddf.com')`,
-            (err2, res2) => {
-              if (!err2) res.send("success");
-              else throw err2;
-            }
-          );
-        } else {
-          throw err1;
-        }
-      });
-    } else {
-      res.send("username already taken!");
+                req.body.username
+              }','${hash}','${new Date()}','https://ddf.com')`,
+              (err2, res2) => {
+                if (!err2) res.send("success");
+                else throw err2;
+              }
+            );
+          } else {
+            throw err1;
+          }
+        });
+      } else {
+        res.send("username already taken!");
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
