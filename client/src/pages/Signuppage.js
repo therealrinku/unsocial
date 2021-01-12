2f5aa1fca084f17d901399b1d678a6b769cf4ee3
rinkuuu <backupfb2032@gmail.com>
2021-01-12 21:11:35 +0545

    validation done

--- client/src/pages/Signuppage.js
+++ client/src/pages/Signuppage.js
@@ -8,16 +8,45 @@ const Signuppage = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const history = useHistory();
 
   const SIGNUP = async (e) => {
     e.preventDefault();
-    const response = await signupUser(email, username, password);
-    if (response !== "success") {
-      alert(response);
+
+    const trimmed = {
+      email: email.trim(),
+      username: username.trim(),
+      password: password.trim(),
+    };
+
+    if (
+      trimmed.email !== "" &&
+      trimmed.email.length >= 8 &&
+      trimmed.email.length <= 40 &&
+      trimmed.email.includes("@")
+    ) {
+      if (
+        trimmed.username.length >= 5 &&
+        trimmed.username.length <= 25 &&
+        !trimmed.username.includes(" ")
+      ) {
+        if (trimmed.password.length >= 8 && trimmed.password.length <= 30) {
+          const response = await signupUser(email, username, password);
+          if (response !== "success") {
+            setError(response);
+          } else {
+            history.push("/login");
+          }
+        } else {
+          setError("Password must be between 8 and 30 characters.");
+        }
+      } else {
+        setError("Username must be spaceless between 5 and 25 characters.");
+      }
     } else {
-      history.push("/login");
+      setError("Please enter a valid email address.");
     }
   };
 
@@ -51,6 +80,7 @@ const Signuppage = () => {
 
           <button type="submit">Signup</button>
           <p>By signing up, you agree to our terms and policies.</p>
+          <p style={{ color: "red" }}>{error}</p>
         </form>
       </div>
 
