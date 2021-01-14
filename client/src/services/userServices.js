50f21d7649930dfda1832f228f30cc5fb9f49a14
rinkuuu <backupfb2032@gmail.com>
2021-01-14 10:14:07 +0545

    get recommended users service added

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 import server_url from "../server_url";
 
+export const getRecommendedUsers = async () => {
+  try {
+    return new Promise((resolve) => {
+      return axios.get(server_url + `/user/getrecommended`).then((res) => {
+        resolve(res.data);
+      });
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const getSearchedUsers = async (query) => {
   try {
     return new Promise((resolve) => {
