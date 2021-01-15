021b671c80d3655f9bff5e83f092a29bfacb727a
rinkuuu <backupfb2032@gmail.com>
2021-01-15 17:09:02 +0545

    get comments service added

--- client/src/services/commentServices.js
+++ client/src/services/commentServices.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 import server_url from "../server_url";
 
+export const getComments = async (post_uid, current_user_uid) => {
+  try {
+    const comments = await axios
+      .get(server_url + `/comment/getcomments/${post_uid}/${current_user_uid}`)
+      .then((res) => res.data);
+    return comments;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const addComment = async (
   comment,
   commenter_uid,
