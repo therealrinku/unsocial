8eb756f26b6da125a35ae129e92720368fe739e9
rinkuuu <backupfb2032@gmail.com>
2021-01-15 17:27:53 +0545

    comments view component added

---
+++ client/src/components/CommentsView.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CommentsView = (comments) => {
+  return (
+    <div className="comments--view">
+      {comments.map((comment) => {
+        return (
+          <div key={comment.comment_uid}>
+            <section>
+              <img src={comment.poster_profile_image} alt="profile-image" />
+              <p>{comment.poster_username}</p>
+            </section>
+
+            <section>
+              <p>{comment.posted_date}</p>
+              <p>{comment.comment_likes_count || 0} likes</p>
+            </section>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CommentsView;
