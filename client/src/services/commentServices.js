b527a91d8f10eaef889d6834aac1d51493ed5c62
rinkuuu <backupfb2032@gmail.com>
2021-01-15 21:05:16 +0545

    shiity issue fixed

--- client/src/services/commentServices.js
+++ client/src/services/commentServices.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { response } from "express";
 import server_url from "../server_url";
 
 export const getComments = async (post_uid, current_user_uid) => {
@@ -21,15 +20,13 @@ export const addComment = async (
   posted_date
 ) => {
   try {
-    const response = await axios
-      .post(server_url + "/comment/addcomment", {
-        comment,
-        commenter_uid,
-        post_uid,
-        post_owner_uid,
-        posted_date,
-      })
-      .then((res) => res.data);
+    const response = await axios.post(server_url + "/comment/addcomment", {
+      comment,
+      commenter_uid,
+      post_uid,
+      post_owner_uid,
+      posted_date,
+    });
     return response;
   } catch (err) {
     throw new Error(err.message);
