1d5660df6681d25096a978bdef17635b95e6114e
rinkuuu <backupfb2032@gmail.com>
2021-01-15 17:01:44 +0545

    getting comments route added

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const db = require("../database/db");
 
+//get comments for selected post
+router.get("/getcomments/:post_uid/:current_user_uid", (req, res) => {
+  db.query(
+    `SELECT username as poster_username,
+    '${req.params.current_user_uid}'=ANY(likers) AS liked_by_me,
+  profile_image_url as poster_profile_image,array_length(likers,1) as comment_likes_count,comment,posted_date
+  FROM comments INNER JOIN users ON (comments.commenter_uid)=(users.uid)::text
+  WHERE post_uid='${req.params.post_uid}'`,
+    (err, res1) => {
+      if (!err) res.send(res1.rows);
+      else throw err;
+    }
+  );
+});
+
 router.post("/addcomment", (req, res) => {
   db.query(
     `INSERT INTO comments(comment,commenter_uid,post_uid,post_owner_uid,posted_date)
