e83eea3a0bb451e55439ab42dda41132f149a6ac
rinkuuu <backupfb2032@gmail.com>
2021-01-17 12:59:28 +0545

    udpate user data route added

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 import server_url from "../server_url";
 
+export const updateUserData = async (username, email, bio) => async () => {
+  try {
+    return new Promise((resolve) => {
+      return axios
+        .post(server_url + `/user/updateprofile`, { username, email, bio })
+        .then((res) => {
+          resolve(res.data);
+        });
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 export const getRecommendedUsers = async (uid) => {
   try {
     return new Promise((resolve) => {
