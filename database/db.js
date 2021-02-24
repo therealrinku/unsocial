ea55e95e3b326427466cdb4e5178f6dca65d360f
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2021-02-24 20:19:33 +0545

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,6 +3,8 @@ const dbUrl = require("./dbUrl");
 
 const db = new pg.Client(dbUrl);
 
+//db url deleted for security reasons. layout is dbUrl="url" and export default dbUrl; that's it.
+
 db.connect()
 	.then(() => {
 		console.log("connected to postgres successfully.");
