6632c0a5cefef094697a2e0208742653ac47f3e0
rinkuuu <backupfb2032@gmail.com>
2021-02-27 08:38:41 +0545

    firebase config moved and ignored

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyC0UB2wAM1IIBgdatyrAy0G2Ie2Qeo0f6I",
+  authDomain: "instacloone.firebaseapp.com",
+  projectId: "instacloone",
+  storageBucket: "instacloone.appspot.com",
+  messagingSenderId: "315649415271",
+  appId: "1:315649415271:web:654e7769f08597d5725cc2",
+  measurementId: "G-ZV1718DND2",
+};
+
+export default config;
