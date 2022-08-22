66c9fcb16f3aa3792a4d866fc6e7fd38c9127c32
rinku.chaudhary <rinku.chaudhary@amniltech.com>
2022-08-22 09:10:36 +0545

    form validation and test cases added for login and register forms

---
+++ client/src/__tests__/Form.test.js
@@ -0,0 +1,33 @@
+import Form from "../components/Form";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+test("If submit button is disabled in login page if username and password are less than 5 characters", () => {
+  render(<Form formType="Login" username="user" password="pass" />);
+  expect(screen.getByText("Login")).toBeDisabled();
+});
+
+test("If submit button is enabled in login page if username and password are more than 5 characters", () => {
+  render(<Form formType="Login" username="username" password="password" />);
+  expect(screen.getByText("Login")).toBeEnabled();
+});
+
+test("if signup form is disabled when email,username or password are less than 5 characters long", () => {
+  render(<Form formType="Register" email="hellfelfllfl" password="dddd" username="d" />);
+  expect(screen.getByText("Register")).toBeDisabled();
+});
+
+// test("If three dots are shown after clicking sign in", () => {
+//   let loading = false;
+//   const handleClick = jest.fn(() => {
+//     console.log("dd");
+//     loading = true;
+//   });
+//   render(
+//     <Form formType="Login" loading={loading} username="username" password="password" onFormSubmit={handleClick} />
+//   );
+//   const submitBtn = screen.getByText("Login");
+//   userEvent.click(submitBtn);
+//   expect(handleClick).toHaveBeenCalledTimes(1);
+//   //expect(submitBtn).toContain("...");
+// });
--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -32,6 +32,23 @@ export default function Form({
   onFormSubmit,
   loading,
 }: Form) {
+  const shouldSubmitButtonBeDisabled = () => {
+    if (loading) return true;
+    if (formType === "Login") {
+      if (username.trim().length < 5 || password.trim().length < 5) {
+        return true;
+      }
+    }
+
+    if (formType === "Register") {
+      if (username.trim().length < 5 || password.trim().length < 5 || (email || "emaild").trim().length < 5) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
   return (
     <div className={styles.formWrapper}>
       <Logo />
@@ -40,36 +57,20 @@ export default function Form({
         {formType === "Register" && (
           <>
             <label>Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => onEmailChange(e.target.value)}
-            />
+            <input type="email" value={email} onChange={(e) => onEmailChange(e.target.value)} />
           </>
         )}
         <label>Username</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => onUsernameChange(e.target.value)}
-        />
+        <input type="text" value={username} onChange={(e) => onUsernameChange(e.target.value)} />
         <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => onPasswordChange(e.target.value)}
-        />
+        <input type="password" value={password} onChange={(e) => onPasswordChange(e.target.value)} />
         {formType === "Register" && (
           <>
             <label>Repeat Password</label>
-            <input
-              type="password"
-              value={repeatPassword}
-              onChange={(e) => onRepeatPasswordChange(e.target.value)}
-            />
+            <input type="password" value={repeatPassword} onChange={(e) => onRepeatPasswordChange(e.target.value)} />
           </>
         )}
-        <button type="submit" disabled={loading}>
+        <button type="submit" disabled={shouldSubmitButtonBeDisabled()}>
           {!loading ? (
             <>{formType === "Login" ? "Login" : "Register"}</>
           ) : (
@@ -84,20 +85,19 @@ export default function Form({
           )}
         </button>
         {formType === "Register" && (
-          <p style={{ fontSize: "13px", color: "gray" }}>
-            By signing up, you agree to our terms and policies.
-          </p>
+          <>
+            <i style={{ fontSize: "13px", color: "blue", marginTop: "8px" }}>
+              Username and password should be minimum 5 characters long.
+            </i>
+            <p style={{ fontSize: "13px", color: "gray" }}>By signing up, you agree to our terms and policies.</p>
+          </>
         )}
         <p style={{ color: "red", fontSize: "13px" }}>{errorMessage}</p>
         <div>
-          <p>
-            {formType === "Register"
-              ? "Already have an account?"
-              : "Don't have an account?"}
-          </p>
-          <Link to={formType === "Register" ? "/login" : "/register"}>
+          <p>{formType === "Register" ? "Already have an account?" : "Don't have an account?"}</p>
+          {/* <Link to={formType === "Register" ? "/login" : "/register"}>
             {formType === "Register" ? "Login" : "Register"}
-          </Link>
+          </Link> */}
         </div>
       </form>
     </div>
