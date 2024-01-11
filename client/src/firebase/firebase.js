526ca0004fbe199dd074b41fad221638c3eedf72
therealrinku <rinkunited2032@gmail.com>
2024-01-11 14:22:12 +0545

    chore: update config

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 
-// this config is outdated, i have updated  it in my local setup 😌
 const firebaseConfig = {
-  apiKey: "AIzaSyCULMQk_Nv61qdPACYDnIXTp0AYfXpV7hw",
-  authDomain: "uns0cial.firebaseapp.com",
-  projectId: "uns0cial",
-  storageBucket: "uns0cial.appspot.com",
-  messagingSenderId: "1020518259432",
-  appId: "1:1020518259432:web:25f223578523f02447c75d",
-  measurementId: "G-3Z3PTVYVDD",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
